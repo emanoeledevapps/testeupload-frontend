04642cee748fb2440a0f0949ce3c13f0748e5691
emanoel augusto <emanoelaugusto7@gmail.com>
2021-03-14 16:11:35 -0300

    frontend

---
+++ src/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #7159c1
+`;
+
+export const Content = styled.div`
+    width: 100%;
+    max-width: 400px;
+    margin: 30px;
+    background: #fff;
+    border-radius: 10px;
+    padding: 20px;
+`;
